c50e2ab7d4a7dfe87bd152146d963c3d4ab192f4
doachy <dnwjd2839@naver.com>
2021-04-18 14:11:58 +0000

    how to send the data by params

--- App.js
+++ App.js
@@ -7,16 +7,34 @@ function HomeScreen({ navigation }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home Screen</Text>
-            <Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
+            <Button
+                title="Go to Details"
+                onPress={() =>
+                    navigation.navigate('Details', {
+                        itemId: 86,
+                        otherParam: 'anything you want here',
+                    })
+                }
+            />
         </View>
     );
 }
 
-function DetailsScreen({navigation}) {
+function DetailsScreen({ navigation }) {
+    const { itemId, otherParam } = route.params;
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Details Screen</Text>
-            <Button title="Go to Details... again" onPress={() => navigation.navigate('Details')} />
+            <Text>itemId: {JSON.stringify(itemId)}</Text>
+            <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+            <Button
+                title="Go to Details... again"
+                onPress={() =>
+                    navigation.push('Details', {
+                        itemId: Math.floor(Math.random() * 100),
+                    })
+                }
+            />
         </View>
     );
 }
