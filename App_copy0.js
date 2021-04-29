4227cd98317743f747ce6bff01d83a65d204c725
doachy <dnwjd2839@naver.com>
2021-04-29 09:31:20 +0000

    test successful

--- App_copy0.js
+++ App_copy0.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
-import { Button, View, Text, ImageBackground, StyleSheet } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import AnalogClock from 'react-native-clock-analog';
 
 function HomeScreen({ navigation }) {
     return (
@@ -44,44 +43,13 @@ const Stack = createStackNavigator();
 
 function App() {
     return (
-      <View style={styles.container}>
-        <ImageBackground
-          source={{
-            uri:
-              'https://i.pinimg.com/originals/62/6f/84/626f84c40696c1308a77fd8331e12b3e.jpg',
-          }}
-          style={{
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: 500,
-            width: 500,
-          }}>
-          <AnalogClock
-            size={100}
-          />
-          <View style={{ marginBottom: 5 }} />
-          <AnalogClock
-            colorClock="#fff"
-            colorNumber="#000000"
-            colorCenter="#000000"
-            colorHour="#000000"
-            colorMinutes="#000000"
-            hour="5"
-            minutes={55}
-          />
-        </ImageBackground>
-      </View>
+        <NavigationContainer initialRouteName="Home">
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Details" component={DetailsScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-});
 
 export default App;
