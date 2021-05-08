147fc42ac9ff5fb7ee09161b0a0c292adc3f6b17
doachy <dnwjd2839@naver.com>
2021-05-08 09:45:25 +0000

    how to use the scorllview like fullscreen

--- App.js
+++ App.js
@@ -1,36 +1,46 @@
 import * as React from 'react';
-import { Button, View, Text, ImageBackground, StyleSheet } from 'react-native';
+import { Button, View, Text, ImageBackground, StyleSheet, SafeAreaView, ScrollView, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AnalogClock from 'idea-native-clock';
+import { LinearGradient } from 'expo-linear-gradient';
 
 function App() {
     return (
-        <View style={styles.container}>
-            <AnalogClock size={100} />
-            <View style={{ marginBottom: 5 }} />
-            <AnalogClock
-                colorClock="#000"
-                colorNumber="#000000"
-                colorCenter="#00BCD4"
-                colorHour="#FF8F00"
-                colorMinutes="#FFC400"
-                hour="3"
-                minutes={55}
-                seconds={60}
-            />
-        </View>
+        <SafeAreaView style={styles.container}>
+			<LinearGradient
+                    // Background Linear Gradient
+                    colors={['#4c669f', '#3b5998', '#192f6a']}
+                    style={styles.background}
+                />
+            <ScrollView style={styles.scrollView}>
+				<Text>hi dsfknsdfkasnlkdfnasldkn</Text>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#ecf0f1',
-        padding: 8,
-    },
+    flex: 1,
+  },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 1000,
+  },
+  button: {
+    padding: 15,
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  text: {
+    backgroundColor: 'transparent',
+    fontSize: 15,
+    color: '#fff',
+  },
 });
 
 export default App;
